e2e6664fa87a280902417da0cf2aa5ca1bf94e9e
Christopher Harper <christopherharper123@gmail.com>
2024-05-07 20:25:19 -0600

    Completed Learning Activity

--- week03/learn/DuplicateCounter.cs
+++ week03/learn/DuplicateCounter.cs
@@ -1,4 +1,7 @@
-﻿public class DuplicateCounter
+﻿using System.Globalization;
+using System.Security.Cryptography.X509Certificates;
+
+public class DuplicateCounter
 {
     //Count how many duplicates are in a collection of data.
 
@@ -24,7 +27,17 @@
 
     private static int CountDuplicates(int[] data)
     {
-        // Add code here.
-        return 0;
+        var duplicateDelete = new HashSet<int>();
+        var duplicateCount = 0;
+
+        foreach (var numbers in data)
+        {
+            if (duplicateDelete.Contains(numbers))
+                duplicateCount++;
+            else
+                duplicateDelete.Add(numbers);
+        }
+
+        return duplicateCount;
     }
 }
